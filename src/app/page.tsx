0a91c87e003a33dd9c328d0c96a7053dc864aaa9
Carlo Yannuzzi <carlogyannuzzi@gmail.com>
2023-09-11 15:19:10 -0400

    more mobile responsive.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,45 +22,53 @@ export default function Home() {
     const  getNewQuote = async () => {
     const request = await fetch('https://api.quotable.io/quotes/random');
     const response = await request.json();
-    console.log('Response from api: ' ,response);
+    // console.log('Response from api: ' ,response);
     setQuote(response);
   }
 
 
   return (
 
-          <div id='wrapper' className='flex items-center container mt-96 mx-auto w-96 '>
+          <div id='wrapper' className='flex items-center container mx-auto mt-80 lg:mt-96  '>
 
             {
               quote && quote.length && quote.map((item:any, index: number) => {
                 const href = `https://twitter.com/intent/tweet?text=${item.content}%20%20%23${item.author}`;
                 return (
-                  <div id='quote-box' className=' container' key={item._id}>
+                  <div id='quote-box' className='container m-8 text-center' key={item._id}>
 
-                  <div id='text' key={index}><FaQuoteLeft className='inline-flex pr-1'/>{item.content} <FaQuoteRight className='inline-flex pl-1'/></div>
-                  <div id='author' key={item.authorSlug} className='mt-3 mb-2  ' > -{item.author}</div>
-                  <div className='flex mt-4 mb-4 '>
+                  <div id='text'
+                       key={index}
+
+                  ><FaQuoteLeft className='inline-flex pr-1'/>{item.content} <FaQuoteRight className='inline-flex pl-1'/></div>
+                  <div id='author' key={item.authorSlug} className='mt-3 mb-2 ' > -{item.author}</div>
+                  <div className='flex space-x-1 container mt-4 mb-4  '>
+
                       <a id='tweet-quote'
                         key={index+"a1"}
                          href={href}
                          target='_blank'
                          className='
+
                                     pl-2
-                                    flex-none
+                                    pt-1
+                                    inline-flex
                                     bg-cyan-500
                                     hover:bg-cyan-700
                                     rounded-full
                                     w-8'><FaTwitter /> </a>
-                  </div>
-
-                  <div><button id='new-quote'
+                        <div><button id='new-quote'
                              onClick={getNewQuote}
-                             className=' px-1
-                                        flex-auto
+                             className='px-auto
+
+                                        inline-flex
                                         rounded-full
                                         bg-cyan-500
                                         hover:bg-cyan-700'>Generate Quote</button></div>
                   </div>
+                  </div>
+
+
                 )
               })
             }
